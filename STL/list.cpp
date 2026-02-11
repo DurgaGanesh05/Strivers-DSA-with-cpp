1c39ab969f5a9795ae66090b1e18697b922b1ff7
Ganesh <seeramdurgaganesh@gmail.com>
2026-02-11 20:46:07 +0530

    chore: clean up empty code change sections in the changes log

--- STL/list.cpp
+++ STL/list.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
-#include <list>
+#include <queue>
+#include <stack>
 using namespace std;
 
 int main()
 {
-
+    queue<int> st;
+    st.push(12);
+    st.push(45);
+    st.push(56);
+    st.push(24);
+    st.push(75);
+    cout << st.back();
     return 0;
 }
