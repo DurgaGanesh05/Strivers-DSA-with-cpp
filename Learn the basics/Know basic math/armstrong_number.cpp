5be6965930c4bbeadfc8fc8f93ab079f127b4a7b
Ganesh <seeramdurgaganesh@gmail.com>
2026-02-13 11:52:18 +0530

    Refactor code structure for improved readability and maintainability

--- Learn the basics/Know basic math/armstrong_number.cpp
+++ Learn the basics/Know basic math/armstrong_number.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
-    int n;
+    int n = 12;
     cin >> n;
 
+    int no=0, rem=0, temp=n, temp1=n;
+    int arm=0;
+
+    while(n>0){
+        rem = n%10;
+        no++;
+        n/=10;
+    }
+
+    rem=0;
+    while(temp>0){
+        rem = temp%10;
+        arm += pow(rem, no);
+        temp /= 10;        
+    }
+
+    if(temp1 == arm) cout << "Armstrong Number";
+    else cout << " Not Armstrong Number";
     return 0;
 }
