20dc2ac6918cc97ebd35d606b0ee2b956059ca72
Ganesh <seeramdurgaganesh@gmail.com>
2026-02-14 21:51:15 +0530

    hashing

---
+++ Learn the basics/Basic Hashing/CountingFrequenciesofArrayElements.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+vector<vector<int>> countFrequencies(vector<int>& nums) {
+        // Your code goes here
+    }
+
+
+int main(){
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for(int i=0; i<n; i++) cin>>arr[i];
+
+    
+    return 0;
+}
---
+++ Learn the basics/Basic Hashing/rough.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+#include<map>
+#include<unordered_map>
+using namespace std;
+
+int main(){
+
+    // int n = 5;
+    // int arr[5]={1, 3, 2, 1, 3};
+
+    // int q=5;
+
+    // int hash[13]={0};
+    // for(int i=0; i<n; i++){
+    //     hash[arr[i]]++;
+    // }
+
+    // while(q--){
+    //     int a;
+    //     cin>>a;
+
+    //     cout<<hash[a]<<endl;
+    // }
+
+
+
+
+    // string s="abcdabehf";
+
+    // int hash[256] = {0};
+
+    // for(int i=0; i<s.length(); i++){
+    //     hash[s[i]]++;
+    // }
+
+    // int q=5;
+    // char c;
+    // while(q--){
+    //     cin>>c;
+
+    //     cout<<hash[c]<<endl;
+    // }
+
+
+
+
+
+    //hashing using maps
+    // int n=7;
+    // int arr[] = {1, 2, 3, 1, 3, 2, 12};
+
+
+    // unordered_map<int, int> mpp;
+    // for(int i=0; i<n; i++){
+    //     mpp[arr[i]]++;
+    // }
+
+    // //just for iterating normally
+    // for(auto i : mpp) cout << i.first << "->" << i.second << endl;
+
+    // int q=5;
+    // while(q--){
+    //     int a;
+    //     cin>>a;
+
+    //     // cout << mpp[a] << endl;
+    // }
+
+
+
+    return 0;
+}
