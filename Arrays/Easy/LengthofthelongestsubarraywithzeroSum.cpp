1943f8b858892552e79a4cf8fafbb589d6638091
Ganesh <seeramdurgaganesh@gmail.com>
2026-02-21 00:41:04 +0530

    clearing drafts

---
+++ Arrays/Easy/LengthofthelongestsubarraywithzeroSum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int> arr = {9, -3, 3, -1, 6, -5};
+    int n = arr.size();
+    int p = 0;
+
+    return 0;
+}
--- Arrays/Easy/SubArray.cpp
+++ Arrays/Easy/SubArray.cpp
@@ -5,29 +5,60 @@ using namespace std;
 
 int main(){
 
-    vector<int> arr = {10, 5, 2, 7, 1, 9};
-    int n = arr.size();
-    int p = 15;
+    // vector<int> arr = {9, -3, 3, -1, 6, -5};
+    // int n = arr.size();
+    // int p = 0;
 
 
-    int sum=0;
-    int diff=0;
-    int maxDiff=0;
+    // int i=0, j=i;
+    // int sum=0, diff=0, maxDiff=0;
 
-    for(int i=0; i<n; i++){
-        for(int j=i; j<n; j++){
-            sum=0;
-            for(int k=i; k<=j; k++){
-                sum+=arr[k];
-            }
-            if(sum == p){
-                diff=j-i+1;
-            }
-            // cout<<diff;
-            maxDiff=max(diff, maxDiff);
-        }
-        cout<<endl;
-    }
-    cout<<maxDiff;
+    // while(i<n && j<n){
+    //     sum+=arr[j];
+
+    //     if(sum == p){
+    //         diff = j-i+1;
+    //         maxDiff = max(maxDiff, diff);
+    //     }
+
+    //     while(sum > p){
+    //         sum = sum-arr[i];
+    //         i++;
+    //     }
+    //     j++;
+    // }
+
+    // cout<<maxDiff;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // int sum=0;
+    // int diff=0;
+    // int maxDiff=0;
+
+    // for(int i=0; i<n; i++){
+    //     sum=0;
+    //     for(int j=i; j<n; j++){
+    //         sum+=arr[j];
+    //         if(sum == p){
+    //             diff=j-i+1;
+    //         }
+    //     }
+    //     maxDiff=max(diff, maxDiff);
+    // }
+    // cout<<maxDiff;
+
+
     return 0;
 }
