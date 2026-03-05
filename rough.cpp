3678dfc8b7a096429b414e2660c6ce2eab5b9ccb
Ganesh <seeramdurgaganesh@gmail.com>
2026-03-05 16:29:22 +0530

    Refactor main function to implement frequency counting of array elements

--- rough.cpp
+++ rough.cpp
@@ -1,27 +1,48 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
 int main()
 {
-    bool flag;
-    vector<int> arr = {1, 2, 3, 1};
-    int k = 3;
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 2, 3};
     int n = arr.size();
+    int ml = INT32_MIN;
 
-    int j;
+    unordered_map<int, int> mpp;
 
-    for (int i = 0; i < n - 1; i++)
-    {
-        j = i + 1;
-        if (arr[i] == arr[j] && abs(i - j) <= k)
-        {
-            flag = true;
-        }
-        else
-        {
-            j++;
-        }
-    }
-    flag = false;
+    for (int i : arr)
+        mpp[arr[i]]++;
+
+    for (auto i : mpp) cout << i.first << " " << i.second << endl;
 }
+
+// int main()
+// {
+//     vector<int> arr = {1};
+//     int k = 0;
+//     int n = arr.size();
+//     int s = 0;
+//     int i = 0, c = 0;
+
+//     for (int j = 0; j < n; j++)
+//     {
+//         s = s + arr[j];
+
+//         cout<<s<<endl;
+
+//         while (s > k)
+//         {
+//             s = s - arr[i];
+//             i++;
+//         }
+//         cout<<s<<endl;
+
+//         if (s == k)
+//         {
+//             c++;
+//         }
+
+//         // cout << c << " " << s << endl;
+//     }
+// }
