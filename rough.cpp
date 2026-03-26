233e108b0fcf0bccc4a2d4f5a5e20a1a2682e8ad
Ganesh <seeramdurgaganesh@gmail.com>
2026-03-27 00:02:51 +0530

    Refactor palindrome checking logic and clean up main function for improved readability

--- rough.cpp
+++ rough.cpp
@@ -6,48 +6,40 @@
 #include <string>
 using namespace std;
 
-
-int isPalindrome(string s, int i, int j){
+bool isPalindrome(string s, int i, int j)
+{
     // int i=0, j=s.length()-1;
 
-    while(i<=j){
-        if(s[i] != s[j]) {
+    while (i <= j)
+    {
+        if (s[i] != s[j])
+        {
             return false;
-        }else {
+        }
+        else
+        {
             i++, j--;
         }
     }
     return true;
 }
 
-
-
 int main()
 {
     string s = "aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga";
 
-    int i = 0, j = s.size() - 1;
+    int i = 0, j = s.length() - 1;
 
-    bool flag = false;
+    bool flag=true;
     while (i < j)
     {
-        int p = 1;
-        if (s[i] == s[j])
-        {
-            flag = true;
-            i++;
-            j--;
-        }
-        else if(s[i] != s[j])
-        {
-            isPalindrome(s, 0, i);
-            isPalindrome(s, j, s.length()-1);
-        }
+        
     }
-    cout << s;
+
+    cout<<flag;
 
     return 0;
-}
+}   
 
 /*
 
