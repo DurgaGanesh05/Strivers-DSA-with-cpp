9a1b48501002d5a5cd3a30de13fd0fb94aa30d69
Ganesh <seeramdurgaganesh@gmail.com>
2026-03-26 22:47:35 +0530

    Refactor palindrome checking logic and update main function to improve clarity and functionality

--- rough.cpp
+++ rough.cpp
@@ -3,45 +3,52 @@
 #include <unordered_map>
 #include <algorithm>
 #include <set>
+#include <string>
 using namespace std;
+
+
+int isPalindrome(string s, int i, int j){
+    // int i=0, j=s.length()-1;
+
+    while(i<=j){
+        if(s[i] != s[j]) {
+            return false;
+        }else {
+            i++, j--;
+        }
+    }
+    return true;
+}
+
+
 
 int main()
 {
-    vector<int> arr = {2,1,-1};
-                    // 0 1 2 3 4 5
-    int n = arr.size();
+    string s = "aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga";
 
-    vector<int> sumLeft(n);
-    vector<int> sumRight(n);
+    int i = 0, j = s.size() - 1;
 
-    sumLeft[0]=arr[0];
-    sumRight[n-1]=arr[n-1];
-
-    for(int i=1; i<n; i++){
-        sumLeft[i] = sumLeft[i-1]+arr[i];
+    bool flag = false;
+    while (i < j)
+    {
+        int p = 1;
+        if (s[i] == s[j])
+        {
+            flag = true;
+            i++;
+            j--;
+        }
+        else if(s[i] != s[j])
+        {
+            isPalindrome(s, 0, i);
+            isPalindrome(s, j, s.length()-1);
+        }
     }
-    for(int i=n-2; i>=0; i--){
-        sumRight[i] = sumRight[i+1]+arr[i];
-    }
+    cout << s;
 
-    for(int i=0; i<n; i++){
-        if(sumLeft[i] == sumRight[i]) cout<<i<<" ";
-    }
+    return 0;
 }
 
 /*
-
-If the array is:
-
-[0, 4, 0, 0, 3, 0, 5, 0, 1, 0]
-Run your algorithm step by step.
-After the first zero moves, what exactly happens to the second zero and the 1?
-Try tracing that carefully — that example exposes the weakness in your current logic.
-
-for (i->n) if arr[i] == 0, then store the index in temp and i will break the loop
-
- if arr[i] != 0 then swap(arr[i], arr[temp]);
-
-if i dolike this it doesnt work but i need one more while inside the loop it is not optimal then how?
 
 */
