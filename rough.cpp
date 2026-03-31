095dacc024ac89c4f5fc3217c197a9fb199c5e9c
Ganesh <seeramdurgaganesh@gmail.com>
2026-03-31 19:51:18 +0530

    Refactor logic to count vowels in a sliding window and find the maximum count within a specified length

--- rough.cpp
+++ rough.cpp
@@ -8,29 +8,28 @@ using namespace std;
 
 int main()
 {
-    string s = "bbbbb";
-    unordered_map<char, int> mpp;
+    string s = "weallloveyou";
+    int k = 7;
+    int i = 0;
+    int c = 0, cmax = INT16_MIN;
+    string a = "";
 
-    int c = 0, i = 0;
     for (int j = 0; j < s.length(); j++)
     {
-        mpp[s[j]]++;
-        while (mpp[s[j]] >= 2)
+        a += s[j];
+        int p=a.length()-1;
+        if (a[p] == 'a' || a[p] == 'e' || a[p] == 'i' || a[p] == 'o' || a[p] == 'u')
+            c++;
+
+        if (j - i + 1 > k)
         {
-            mpp[s[i]]--;
-            if (mpp[s[i]] == 0)
-                mpp.erase(s[i]);
+            if (a[0] == 'a' || a[0] == 'e' || a[0] == 'i' || a[0] == 'o' || a[0] == 'u')
+                c--;
+            a.erase(a.begin());
             i++;
         }
-        c = max(c, j - i + 1);
+
+        cmax = max(cmax, c);
     }
-
-    cout<<c;
-
-
-    // for(auto i:mpp)cout<<i.first<<"->"<<i.second<<endl;
+    cout<< cmax;
 }
-
-/*
-
-*/
