b044f7c8c3c18fddd5874b99845376e0b1a58ed1
Ganesh <seeramdurgaganesh@gmail.com>
2026-04-03 21:58:39 +0530

    Refactor main function to implement logic for finding the longest substring with case-insensitive character matches

--- rough.cpp
+++ rough.cpp
@@ -7,6 +7,19 @@
 #include <string>
 using namespace std;
 
-int main(){
 
+
+int main(){
+    string s="YazaAay";
+    string ans="";
+
+    for(int i=0; i<s.length(); i++){
+        for(int j=i+1; j<s.length(); j++){
+            if((abs(s[i]-s[j]) == 32) || abs(s[i]-s[j]) == 0) {
+                string temp=s.substr(i, j-i+1);
+                if(temp.length() > ans.length())ans=temp;
+            }
+        }
+    }
+    cout<<ans;
 }
