a95272d50f52f6990ed541d77d0e4d795768ad43
Ganesh <seeramdurgaganesh@gmail.com>
2026-04-05 00:07:49 +0530

    Refactor main function to implement sliding window technique for substring anagram detection

--- rough.cpp
+++ rough.cpp
@@ -7,29 +7,40 @@
 #include <string>
 using namespace std;
 
+int main()
+{
+    string s = "cbaebabacd", p = "abc";
 
+    vector<int> ph(26, 0);
+    vector<int> sh(26, 0);
 
-int main(){
-    string s = "anagram", t = "nagaram";
+    vector<int> ans;
 
-    map<char, int> a;
-    map<char, int> b;
-
-    for(int i=0; i<s.length(); i++){
-        a[s[i]]++;
+    for (int i = 0; i < p.length(); i++)
+    {
+        ph[p[i] - 'a']++;
     }
-    for(int i=0; i<t.length(); i++){
-        b[t[i]]++;
-    }
+    int i=0;
+    for (int j = 0; j < s.length(); j++)
+    {
+        sh[s[j] - 'a']++;
 
-    
-
-    bool flag;
-    if(s.length() == t.length()){
-        for(int i=0; i<s.length(); i++){
-            flag = (a==b);
+        if ((j - i + 1) > p.length())
+        {
+            sh[s[i]-'a']--;
+            i++;
         }
-    }
 
-    cout<<flag;
+        if ((j - i + 1) == p.length() && ph == sh)
+            ans.push_back(i);
+    }
+    for(int i:ans)cout<<i<<" ";
+
+    // sh.push_back();
+    // sh[0]=1;
+    // sh[2]=1;
+    // sh[3]=3;
+    // sh[4]=1;
+    // sh[5]=4;
+    // for(int i:sh)cout<<i<<" ";
 }
