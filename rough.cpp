47433d1b9aabb88e254c0de22de1f9377a557a68
Ganesh <seeramdurgaganesh@gmail.com>
2026-04-09 22:41:20 +0530

    Remove unused code for digit sum calculation in main function

--- rough.cpp
+++ rough.cpp
@@ -9,24 +9,5 @@ using namespace std;
 
 int main()
 {
-    string a = "99", b="99";
-    int p = stoi(a);
-    int q = stoi(b);
-
-    int s1=0, s2=0, rem1=0, rem2=0;
-    while(p>0 && q>0){
-        rem1=p%10;
-        rem2=q%10;
-        
-        s1+=rem1;
-        s2+=rem2;
-
-        p/=10;
-        q/=10;
-    }
-
-    string x=to_string(s1);
-    string y=to_string(s2);
-
-    cout<<x+y;
+    
 }
