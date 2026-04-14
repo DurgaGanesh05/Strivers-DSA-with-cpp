e9bde2b9d36a1d157b00df858f9c6bc478efb1d6
Ganesh <seeramdurgaganesh@gmail.com>
2026-04-14 22:55:39 +0530

    Remove binary search implementation and related code from main function

--- rough.cpp
+++ rough.cpp
@@ -9,48 +9,5 @@ using namespace std;
 
 int main()
 {
-    vector<int> arr = {1};
-    int t = 1;
-
-    vector<int> ans;
-
-    int st = 0, end = arr.size() - 1;
-
-    while (st <= end)
-    {
-        int mid = st + (end - st) / 2;
     
-        if (arr[mid] == t)
-        {
-            if (mid+1 < arr.size() && arr[mid + 1] == t)
-            {
-                ans.push_back(mid + 1);
-                ans.push_back(mid);
-                break;
-            }
-            else if (mid-1 >=0 && arr[mid - 1] == t)
-            {
-                ans.push_back(mid - 1);
-                ans.push_back(mid);
-                break;
-            }
-            else{
-                ans.push_back(mid);
-                ans.push_back(mid);
-                break;
-            }
-        }
-        else if (arr[mid] >= t)
-            end = mid - 1;
-        else
-            st = mid + 1;
-    }
-
-    if (ans.empty())
-    {
-        ans.push_back(-1);
-        ans.push_back(-1);
-    }
-
-    for(int i:ans)cout<<i<<" "; 
 }
