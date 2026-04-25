d313909ba0c79891de47308fe864c02390efcb5b
Ganesh <seeramdurgaganesh@gmail.com>
2026-04-25 21:54:11 +0530

    Refactor code in rough.cpp for improved readability and structure

--- rough.cpp
+++ rough.cpp
@@ -5,11 +5,29 @@
 #include <algorithm>
 #include <set>
 #include <string>
+#include <cmath>
 using namespace std;
-
-
 
 int main()
 {
-       
+    vector<int> arr = {3, 6, 7, 11};
+    int h = 8;
+
+    int sum = 0;
+    double k = 1;
+
+    while (sum <= k)
+    {
+        for (int i = 0; i < arr.size(); i++)
+        {
+            sum += ceil(arr[i] / k);
+        }
+        // cout << sum;
+        if (sum > h)
+        {
+            sum = 0;
+            k++;
+        }
+    }
+    cout<<k;
 }
